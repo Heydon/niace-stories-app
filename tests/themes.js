2027e1b868aa98f17ab6c468d553ce503e57d2da
Robert Preus-MacLaren <rpprroger@googlemail.com>
2014-09-16 17:10:12 +0100

    Fixed some spacing weirdness

--- tests/themes.js
+++ tests/themes.js
@@ -2,16 +2,16 @@
 var assert = require('assert');
 
 suite('Themes', function() {
-  test('in the server', function(done, server) {
-    server.eval(function() {
-      Themes.insert({title: 'hello title'});
-      var docs = Themes.find().fetch();
-      emit('docs', docs);
-    });
+	test('in the server', function(done, server) {
+		server.eval(function() {
+			Themes.insert({title: 'hello title'});
+			var docs = Themes.find().fetch();
+			emit('docs', docs);
+		});
 
-    server.once('docs', function(docs) {
-      assert.equal(docs.length, 1);
-      done();
-    });
-  });
+		server.once('docs', function(docs) {
+			assert.equal(docs.length, 1);
+			done();
+		});
+	});
 });
