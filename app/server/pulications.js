83a0f1a66128f9d7b4ae24f3df3009eddc8f21ca
Robert Preus-MacLaren <rpprroger@googlemail.com>
2014-09-24 15:24:38 +0100

    Implemented fixtures for the travis deploy and moved themes and fixtures to new data folder

--- app/server/pulications.js
+++ app/server/pulications.js
@@ -11,25 +11,27 @@ Meteor.publish('themes', function() {
 // Perhaps look to remove these fixture blocks when releasing?..
 if( Stories.find().count() === 0 ) {
 	try { // 
-		var stories = JSON.parse( Assets.getText('stories/stories.json') );
+		console.log( 'Attempting to import stories from private/data/stories.json' );
+		var stories = JSON.parse( Assets.getText('data/stories.json') );
 		_.each( stories, function( story, i ) {
-			console.log( '\rImporting story ' + (i + 1) + '/' + stories.length );
+			console.log( 'Importing story ' + (i + 1) + '/' + stories.length );
 			Stories.insert( story );
 		});
 	} catch(e) {
-		console.log( 'couldn\'t load private/stories/stories.json' );
+		console.log( 'couldn\'t load private/data/stories.json' );
 	}
 }
 
 // Fixture for themes if database empty
 if( Themes.find().count() === 0 ) {
 	try { // 
-		var themes = JSON.parse( Assets.getText('themes/themes.json') );
-		_.each( themes, function( story, i ) {
-			console.log( '\rImporting theme ' + (i + 1) + '/' + themes.length );
-			Stories.insert( story );
+		console.log( 'Attempting to import themes from private/data/themes.json' );
+		var themes = JSON.parse( Assets.getText('data/themes.json') );
+		_.each( themes, function( theme, i ) {
+			console.log( 'Importing theme ' + (i + 1) + '/' + themes.length );
+			Themes.insert( theme );
 		});
 	} catch(e) {
-		console.log( 'couldn\'t load private/themes/themes.json' );
+		console.log( 'couldn\'t load private/data/themes.json' );
 	}
 }
