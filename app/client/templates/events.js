18186400443458bc434bd852bf5198be68d6ad54
Heydon <heydon@heydonworks.com>
2014-12-09 16:19:07 +0000

    initial branding overhaul

--- app/client/templates/events.js
+++ app/client/templates/events.js
@@ -2,3 +2,33 @@
  * File for small collections of events
  * Move large helpers and stuff into client/features/feature.js
  */
+
+var showSubmenu = function(dropdown) {
+	console.log('show');
+	dropdown.attr('aria-hidden', 'false');
+};
+
+var hideSubmenu = function(dropdown) {
+  dropdown.attr('aria-hidden', 'true');
+};
+
+Template.banner.events({
+	'mouseenter [aria-haspopup]': function(e) {
+		showSubmenu($(e.currentTarget).next());
+	},
+	'focus .info > a': function() {
+		hideSubmenu($('[aria-label="submenu"]'));
+	},
+	'click [aria-haspopup]': function(e) {
+		e.preventDefault();
+		var submenu = $(e.currentTarget).next();
+		showSubmenu(submenu);
+		$(submenu).find('li:first-child a').focus();	
+	},
+	'mouseleave .info': function(e) {
+		hideSubmenu($(e.currentTarget).find('[aria-label="submenu"]'));
+	},
+	'blur [aria-label="submenu"] li:last-child a': function() {
+		hideSubmenu($('[aria-label="submenu"]'));
+	}
+});
