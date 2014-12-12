dedb2d770315c70bbc5f62f8e6e55e38968d44c8
Robert Preus-MacLaren <rpprroger@googlemail.com>
2014-12-12 16:06:25 +0000

    Fixed hinting

--- app/server/preload.js
+++ app/server/preload.js
@@ -10,40 +10,35 @@
  * @param  {string} file the file path from app/private/
  * @param  {Collection} Type the collection constructor
  */
-// function preLoadDatabase( file, Type ) {
-// 	try { // 
-// 		console.log( 'Attempting to import database entries from private/' + file );
-// 		var entries = JSON.parse( Assets.getText( file ) );
-// 		_.each( entries, function( data, i ) {
-// 			console.log( 'Importing data from ' + file + ' ' + (i + 1) + '/' + entries.length );
-// 			Type.insert( data );
-// 		});
-// 	} catch(e) {
-// 		console.log( 'couldn\'t load private/' + file );
-// 	}
-// }
+function preLoadDatabase( file, Type ) {
+	try { // 
+		console.log( 'Attempting to import database entries from private/' + file );
+		var entries = JSON.parse( Assets.getText( file ) );
+		_.each( entries, function( data, i ) {
+			console.log( 'Importing data from ' + file + ' ' + (i + 1) + '/' + entries.length );
+			Type.insert( data );
+		});
+	} catch(e) {
+		console.log( 'couldn\'t load private/' + file );
+	}
+}
 
 // Perhaps look to remove these fixture blocks when releasing?..
-// if( Stories.find().count() === 0 ) {
-// 	preLoadDatabase('data/stories.json', Stories);
-// 	preLoadDatabase('data/real_stories.json', Stories);
-// }
+if( Stories.find().count() === 0 ) {
+	preLoadDatabase('data/stories.json', Stories);
+	preLoadDatabase('data/real_stories.json', Stories);
+}
 
 // Fixture for resources if database empty
-// if( Glossary.find().count() === 0 ) {
-// 	preLoadDatabase('data/glossary.json', Glossary);
-// }
+if( Glossary.find().count() === 0 ) {
+	preLoadDatabase('data/glossary.json', Glossary);
+}
 
-// // Fixture for themes if database empty
-// if( Themes.find().count() === 0 ) {
-// 	preLoadDatabase('data/themes.json', Themes);
-// }
+// Fixture for themes if database empty
+if( Themes.find().count() === 0 ) {
+	preLoadDatabase('data/themes.json', Themes);
+}
 
-// // Fixture for resources if database empty
-// if( Resources.find().count() === 0 ) {
-// 	preLoadDatabase('data/resources.json', Resources);
-// }
-
-// if( Alerts.find().count() === 0 ) {
-// 	preLoadDatabase('data/alerts.json', Alerts);
-// }
+if( Alerts.find().count() === 0 ) {
+	preLoadDatabase('data/alerts.json', Alerts);
+}
