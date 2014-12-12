0bfb6497f40c916ff79841f0bf57d9fb284f28c8
Heydon <heydon@heydonworks.com>
2014-12-12 17:03:39 +0000

    soundcloud basic support

--- app/client/main.js
+++ app/client/main.js
@@ -3,3 +3,7 @@ Meteor.startup(function() {
 	Meteor.subscribe('glossary');
 	Meteor.subscribe('config');
 });
+
+var Recordings = new FS.Collection("recordings", {
+  stores: [new FS.Store.FileSystem("recordings", {path: "~/recordings"})]
+});
