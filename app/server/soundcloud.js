bfb9e5db9434fd5c3ab7daa9438aad13164f9800
Robert Preus-MacLaren <rpprroger@gmail.com>
2014-12-15 16:46:10 +0000

    Fixed typo

--- app/server/soundcloud.js
+++ app/server/soundcloud.js
@@ -1,8 +1,8 @@
-if( process.env.SC_CLIENT_ID && process.evn.SC_CLIENT_SECRET ) {
+if( process.env.SC_CLIENT_ID && process.env.SC_CLIENT_SECRET ) {
 
 	var sc = Npm.require('soundclouder');
 
-	sc.init( process.env.SC_CLIENT_ID, process.evn.SC_CLIENT_SECRET, 'http://somefakepath.not-a-domain' );
+	sc.init( process.env.SC_CLIENT_ID, process.env.SC_CLIENT_SECRET, 'http://somefakepath.not-a-domain' );
 
 } else {
 	console.warn( 'Soundcloud connectevity requires SC_CLIENT_ID and a SC_CLIENT_SECRET in the application\'s environment' );
