5f187204cdcd215c5f8c618fe0d844de2adcbe48
Heydon <heydon@heydonworks.com>
2014-12-18 15:25:40 +0000

    branding changes #1

--- app/lib/router.js
+++ app/lib/router.js
@@ -10,7 +10,7 @@ Router.configure({
 			}, 1000);
 			if( !animating ) {
 				animating = true;
-				$('body').animate({scrollTop:0}, '400', function() {
+				$('body').animate({scrollTop:0}, '600', function() {
 					animating = false;
 				});
 			}
@@ -52,6 +52,9 @@ Router.map(function() {
 
 	this.route('glossary', {
 		path: '/glossary'
+	});
+	this.route('about', {
+		path: '/about'
 	});
 	this.route('/story/:_id', {
 		name: 'story',
