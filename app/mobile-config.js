e8e2c4f849c0e958e3ad5baf5a913ab0299af4d8
Robert Preus-MacLaren <rpprroger@googlemail.com>
2015-06-04 10:56:42 +0100

    Actually corrected the template image sizes

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -26,7 +26,8 @@ App.icons({
 
 App.launchScreens({
 	// iOS
-	'iphone': 'resources/splash/splash-320x480.png',
+	// https://github.com/phonegap/phonegap/wiki/App-Splash-Screen-Sizes#dimensions-4
+	'iphone': 'resources/splash/splash-320x480.png', //
 	'iphone_2x': 'resources/splash/splash-320x480@2x.png',
 	'iphone5': 'resources/splash/splash-320x568@2x.png',
 	'ipad_portrait': 'resources/splash/splash-768x1024.png',
@@ -35,14 +36,16 @@ App.launchScreens({
 	'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
 
 	// Android
-	'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-	'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
-	'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-	'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-	'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-	'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-	'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-	'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+	// helpful resource
+	// https://github.com/phonegap/phonegap/wiki/App-Splash-Screen-Sizes#dimensions
+	'android_ldpi_portrait': 'resources/splash/splash-200x320.png', //
+	'android_ldpi_landscape': 'resources/splash/splash-320x200.png', //
+	'android_mdpi_portrait': 'resources/splash/splash-320x480.png', //
+	'android_mdpi_landscape': 'resources/splash/splash-480x320.png', //
+	'android_hdpi_portrait': 'resources/splash/splash-480x800.png', //
+	'android_hdpi_landscape': 'resources/splash/splash-800x480.png', // 
+	'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png', //
+	'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png' //
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
